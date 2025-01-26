5fb7b207caf8e38bab5158f265479b97229a496c
zahraaAed <zahraaalaaeddine94@gmail.com>
2025-01-26 17:18:14 +0200

    adding date and modifying some css in sidebar

--- Habit-Tracker/src/Components/sidebar.jsx
+++ Habit-Tracker/src/Components/sidebar.jsx
@@ -1,15 +1,28 @@
 import React from "react";
-import "../assets/sidebar.css"; // Import the sidebar CSS file
+import "./sidebar.css"; 
 
 const Sidebar = () => {
+  const currentDate = new Date();
+  const dayName = currentDate.toLocaleDateString(undefined, { weekday: "long" });
+  const day = currentDate.getDate(); 
+  const month = currentDate.toLocaleDateString(undefined, { month: "long" });
+  const year = currentDate.getFullYear();
+
   return (
     <div className="sidebar">
+      <div className="date">
+        <p>
+          {dayName} {day} {month}, {year}
+        </p> 
+      </div>
+      <div className="status">
       <h2>Status</h2>
       <ul>
         <li>Completed</li>
         <li>Incompleted</li>
         <li>Progress</li>
       </ul>
+    </div>
     </div>
   );
 };
